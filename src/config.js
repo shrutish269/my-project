ecee023d8cedc98db3a43d2445571068e9c50c61
Shruti Sharma <23bai70539>
2025-11-18 12:01:47 +0530

    Remove node_modules from Git repository

--- src/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const MONGO_URI = process.env.MONGO_URI;
-export const PORT = process.env.PORT || 4000;
-export const JWT_SECRET = process.env.JWT_SECRET;
--- src/utils/errors.js
+++
@@ -1,6 +0,0 @@
-export class AppError extends Error {
-  constructor(message, code) {
-    super(message);
-    this.code = code;
-  }
-}
